198f00da4a8a9058aaa1621c24f8b57111cd842f
McMorris <mcmorrisj@debian-9.3.0-template>
2018-05-20 17:49:16 -0700

    file_search check to ensure two parameters

--- file_search.c
+++ file_search.c
@@ -2,6 +2,13 @@
 
 int main(int argc, char* argv[])
 {
+	if(argc != 3)
+	{
+		printf("Invalid use of file_search: file_search <file_" \
+			"search> <starting directory>\n");
+		return -1;
+	}
+
 	printf("Hello\n");
 	return 0;
 }
