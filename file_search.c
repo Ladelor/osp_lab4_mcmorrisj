ec6c5a6c9c4e5ab6ef278b3447f1cd64715d7300
McMorris <mcmorrisj@debian-9.3.0-template>
2018-05-20 17:58:33 -0700

    Adds more input checking to file_search
    Makes sure starting directory is an absolute path

--- file_search.c
+++ file_search.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char* argv[])
 {
 	if(argc != 3)
 	{
-		printf("Invalid use of file_search: file_search <file_" \
-			"search> <starting directory>\n");
+		printf("Invalid use of file_search: <file_search>" \
+		" <starting directory>\n");
+		return -1;
+	}
+
+	if(argv[2][0] != '/')
+	{
+		printf("Starting Directory must be absolute path\n");
+		return -1;
+	}
+
+	if(argv[2][strlen(argv[2]) - 1] == '/')
+	{
+		printf("Starting Directory must be absolute path\n");
 		return -1;
 	}
 
